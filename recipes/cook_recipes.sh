70e42e632638112871df31b2ff86ffed7ea9fc0e
Elliot Saba <staticfloat@gmail.com>
2013-07-15 10:04:58 -0700

    Can't use the same bash substitution on OSX as we can on linux, use sed instead

--- recipes/cook_recipes.sh
+++ recipes/cook_recipes.sh
@@ -13,7 +13,7 @@ COMMIT=$2
 mkdir -p $JULIA_PKGDIR
 
 for recipe in $(ls build_*.sh); do
-    flavor=${recipe:6:-3}
+    flavor=$(echo $recipe | sed -E 's/build_(.*)\.sh/\1/g')
 
     # create git repo if not already created
     if [[ ! -d $REPOS_DIR/julia-$flavor ]]; then
@@ -31,9 +31,21 @@ for recipe in $(ls build_*.sh); do
     # Run recipe to build this julia commit
     $RECIPE_DIR/build_$flavor.sh 2>&1 | tee ../julia-${flavor}_build.log
 
+    # Grab some metadata about this build real quick
+    export JULIA_FLAVOR=$flavor
+    if [[ "$(uname)" == "Darwin" ]]; then
+        export JULIA_COMMIT_DATE=$(date -jr $(git log -1 --pretty=format:%ct) -u '+%Y-%m-%d %H:%M:%S%Z)
+    else
+        export JULIA_COMMIT_DATE=$(date --date=$(git log -1 --pretty=format:%ct) -u '+%Y-%m-%d %H:%M:%S%Z)
+    fi
+    export JULIA_BRANCH=$BRANCH
+
+    # Run our new performance testsuite!
+    cd tests/perf
+    make codespeed
+
     popd >/dev/null
 
     # Run this julia build on all the tests by invoking taste_flavor.jl
-    export JULIA_FLAVOR=$flavor
-    $REPOS_DIR/julia-$flavor/julia $RECIPE_DIR/taste_flavor.jl $flavor $BRANCH $COMMIT
+    #$REPOS_DIR/julia-$flavor/julia $RECIPE_DIR/taste_flavor.jl $flavor $BRANCH $COMMIT
 done
