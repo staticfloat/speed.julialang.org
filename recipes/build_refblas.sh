691c8de4736908413f629606cf99a7e36aa3fef6
Elliot Saba <staticfloat@gmail.com>
2013-07-16 00:00:59 -0700

    Rename recipes yet again, add in some compatibility for OSX

--- recipes/build_refblas.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Really basic julia installation, ripped straight from Travis compilation procedure
-
-BUILDOPTS="LLVM_CONFIG=llvm-config-3.2 USE_BLAS64=0"
-for lib in LLVM ZLIB SUITESPARSE ARPACK BLAS FFTW LAPACK GMP MPFR PCRE LIBUNWIND READLINE GRISU OPENLIBM RMATH; do
-    export BUILDOPTS="$BUILDOPTS USE_SYSTEM_$lib=1"
-done
-make $BUILDOPTS
-
--- recipes/build_sysblas.sh
+++ recipes/build_sysblas.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# julia installation that uses julia-compiled openblas
+# julia installation that uses system-provided blas
 
 BUILDOPTS="USE_BLAS64=0"
 if [[ "$(uname)" == "Darwin" ]]; then
-    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE"
+    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE BLAS"
 elif [[ "$(uname)" == "Linux" ]]; then
     BUILDOPTS="LLVM_CONFIG=llvm-config-3.2"
-    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE LIBUNWIND READLINE GRISU OPENLIBM RMATH"
+    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE LIBUNWIND READLINE GRISU OPENLIBM RMATH BLAS LAPACK"
 fi
 for lib in SYS_LIBS; do
     export BUILDOPTS="$BUILDOPTS USE_SYSTEM_$lib=1"
 done
 make $BUILDOPTS
-
