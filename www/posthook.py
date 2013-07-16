544c153175e5f2e3f5657f96d97a5b8d41160758
Elliot Saba <staticfloat@gmail.com>
2013-07-16 21:20:14 +0000

    Add in some debugging stuff for travis authentication

--- www/posthook.py
+++ www/posthook.py
@@ -2,12 +2,18 @@ import web
 import subprocess
 import json
 import urllib2
+from hashlib import sha256
+
+# This is the travis token
+from secret_key import *
 
 # This file, when properly configured with uwsgi and nginx, is invoked when Travis completes
 # a successful build.  It then kicks off a compilation and benchmark run to calculate performance
 # of that specific commit on this machine
 
 urls = ('/.*', 'hooks')
+# These are the only branches we'll accept from Travis
+whitelisted_branches = ['master', 'release-0.1'];
 
 app = web.application(urls, globals())
 
@@ -21,10 +27,11 @@ class hooks:
         commit = data['commit']
         branch = data['branch']
 
-        # Note, if you change the bash script to a different recipe_build_area, you must change this one too!
-        open("/tmp/julia_recipe_build_area/incoming/%s"%(commit), "w").write(branch)
+        if branch in whitelisted_branches:
+            # Note, if you change the bash script to a different recipe_build_area, you must change this one too!
+            open("/tmp/julia_recipe_build_area/incoming/%s"%(commit), "w").write(branch)
 
-        # Now, run benchmark.sh with those two arguments!
-        #subprocess.Popen(["../recipes/cook_recipes.sh", branch, commit])
+        open('/tmp/travis-request.txt', 'w').write(urllib2.unquote(web.data()))
+        open('/tmp/travis-headers.txt', 'w').write(str(web.ctx.env))
 
 application = app.wsgifunc()
