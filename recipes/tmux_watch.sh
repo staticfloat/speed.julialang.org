8ff0cd56645cc1a718c8b7ff0411ef4197c3edbd
Elliot Saba <staticfloat@gmail.com>
2013-07-19 13:41:53 -0400

    Nice little script to allow us to launch ./watch_commit_folder.sh along with some panes watching nginx and uwsgi logs...

---
+++ recipes/tmux_watch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# A convenience script to start up tmux shells running watch_commit_folder, along with nginx and uwsgi logs
+
+source common.sh
+
+if [[ -z "$(which tmux)" ]]; then
+    echo "ERROR: tmux not installed!" 1>&2
+    exit 1
+fi
+
+# nest tmux sessions with care....
+OLD_TMUX=$TMUX
+unset TMUX
+
+tmux new-session -d -s "watch"
+tmux new-window -d -k -n "watch_commit" -t "watch:0"
+tmux send-keys -t "watch:0" "cd $RECIPE_DIR; ./watch_commit_folder.sh" C-m
+
+UWSGI_LOG="/tmp/uwsgi_travis-hook.log"
+NGINX_LOG="/var/log/nginx/access.log"
+if [[ "$(uname)" == "Darwin" ]]; then
+    UWSGI_LOG="/usr/local/var/log/uwsgi_travis-hook.log"
+    NGINX_LOG="/usr/local/var/log/nginx"
+fi
+
+tmux new-window -d -n "logs" -t "watch:1"
+tmux send-keys -t "watch:1" "tail -f $UWSGI_LOG" C-m
+tmux split-window -d -v -t "watch:1"
+tmux send-keys -t "watch:1.1" "tail -f $NGINX_LOG" C-m
+
+export TMUX=$OLD_TMUX
