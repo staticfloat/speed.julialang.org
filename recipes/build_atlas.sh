87fd0a58654a66fac8c8e2ce511c3c83d502bf08
Elliot Saba <staticfloat@gmail.com>
2013-07-22 17:23:47 -0700

    First draft of recipe to build an ATLAS-backed Julia

---
+++ recipes/build_atlas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# julia installation that uses julia-compiled openblas
+
+BUILDOPTS="USE_BLAS64=0 USE_ATLAS=1 USE_QUIET=0"
+if [[ "$(uname)" == "Darwin" ]]; then
+    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE"
+elif [[ "$(uname)" == "Linux" ]]; then
+    BUILDOPTS="$BUILDOPTS LLVM_CONFIG=llvm-config-3.2"
+    SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE LIBUNWIND READLINE GRISU OPENLIBM RMATH"
+fi
+for lib in $SYS_LIBS; do
+    export BUILDOPTS="$BUILDOPTS USE_SYSTEM_$lib=1"
+done
+echo make $BUILDOPTS
+make $BUILDOPTS
