e11b327b2a8c85f93f2bdd1c2f31bf8969a2c706
Elliot Saba <staticfloat@gmail.com>
2013-10-09 17:41:24 -0700

    Move temporary directory out of /tmp

--- recipes/common.sh
+++ recipes/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS_DIR=/tmp/julia_recipe_build_area/
+REPOS_DIR=$(echo ~)/tmp/julia_recipe_build_area/
 COMMITS_DIR=$REPOS_DIR/incoming
 RECIPE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export JULIA_PKGDIR=$REPOS_DIR/pkgdir
