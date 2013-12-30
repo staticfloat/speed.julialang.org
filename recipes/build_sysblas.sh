efbd2004edae4188534bea0ae3af98642869838f
Elliot Saba <staticfloat@gmail.com>
2013-12-29 22:52:34 -0500

    Add in LLVM_LLC for Linux computers, move from LLVM 3.2 -> 3.3

--- recipes/build_sysblas.sh
+++ recipes/build_sysblas.sh
@@ -5,7 +5,7 @@ BUILDOPTS="USE_BLAS64=0 USE_QUIET=0"
 if [[ "$(uname)" == "Darwin" ]]; then
     SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE BLAS"
 elif [[ "$(uname)" == "Linux" ]]; then
-    BUILDOPTS="$BUILDOPTS LLVM_CONFIG=llvm-config-3.2"
+    BUILDOPTS="$BUILDOPTS LLVM_CONFIG=llvm-config-3.3 LLVM_LLC=llc-3.3"
     SYS_LIBS="LLVM ZLIB FFTW GMP MPFR PCRE LIBUNWIND READLINE GRISU RMATH BLAS LAPACK"
 fi
 for lib in $SYS_LIBS; do
